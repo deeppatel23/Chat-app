e931fbe73f8f2867c339d29b3a5f4adb31e62366
Deep Patel <deeppatel@Deeps-MacBook-Air.local>
2023-03-06 14:24:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.listen(3000)
