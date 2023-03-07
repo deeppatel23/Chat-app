8e2c2a9f49544db3305e74c9aa5e840db255dd78
Deep Patel <deeppatel@Deeps-MacBook-Air.local>
2023-03-07 15:47:07 +0530

    connect and disconnect

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.set('view engine', 'ejs')
+app.get('/', function(req, res){ 
+   res.sendFile('/Users/deeppatel/Desktop/Work/chat-app/index.html');
+});
 
-app.use(express.static('public'))
+//Whenever someone connects this gets executed
+io.on('connection', function(socket){
+   console.log('A user connected');
+   
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
-
-app.listen(3000)
+http.listen(3000, function(){
+   console.log('listening on *:3000');
+});
